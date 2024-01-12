be8abd1e9bc62087914a488dc7fb02cfb3d1cdb4
Jonatas98Lopes <jonatas.lopes98@outlook.com>
2024-01-12 16:06:20 -0300

    adicionando nova página a planilha

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ print('Para começar, vamos criar uma nova página dentro de uma planilha.')
 
 workbook = openpyxl.Workbook()
 del workbook['Sheet']
+
+while True:
+    new_sheet_name = input('Digite o nome da página: ')
+    workbook.create_sheet(new_sheet_name)
+    
+    add_another_sheet = input('Criar mais uma página nesta planilha?(s/n): ')
+    if add_another_sheet == 'n':
+        break
+
+# Apenas para teste da planilha
+workbook.save('dados.xlsx')
